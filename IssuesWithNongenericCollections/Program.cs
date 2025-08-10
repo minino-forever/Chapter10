f40b38020f9bb69ec243caf1c749f1406c13ee3e
AlexDev <minino-forever@rambler.ru>
2025-08-10 12:11:57 +0700

    create project IssuesWithNongenericCollections

---
+++ IssuesWithNongenericCollections/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IssuesWithNongenericCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
