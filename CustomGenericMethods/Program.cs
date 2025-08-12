41a8beb0514f2f82dbba41fd2623dfdf9c9f3007
AlexDev <minino-forever@rambler.ru>
2025-08-12 16:31:28 +0700

    add project CustomGenericMethods

---
+++ CustomGenericMethods/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace CustomGenericMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Fun with Custom Generic Methods*****\n");
+
+            int a = 10, b = 90;
+
+            Console.WriteLine($"Before swap: {a}, {b}");
+
+            SwapFunctions.Swap<int>(ref a, ref b);
+
+            Console.WriteLine($"After swap: {a}, {b}\n");
+
+            string s1 = "Hello", s2 = "There";
+
+            Console.WriteLine($"Before swap: {s1}, {s2}");
+
+            SwapFunctions.Swap<string>(ref s1, ref s2);
+
+            Console.WriteLine($"After swap: {s1}, {s2}\n");
+
+            bool b1 = true, b2 = false;
+
+            Console.WriteLine($"Before swap: {b1}, {b2}");
+
+            SwapFunctions.Swap<bool>(ref b1, ref b2);
+
+            Console.WriteLine($"After swap: {b1}, {b2}\n");
+
+            DisplayBaseClass<int>();
+
+            DisplayBaseClass<string>();
+        }
+
+        static void DisplayBaseClass<T>()
+        {
+            Console.WriteLine($"Base class of {typeof(T)} is: {typeof(T).BaseType}");
+        }
+    }
+}
---
+++ CustomGenericMethods/SwapFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomGenericMethods
+{
+    public class SwapFunctions
+    {
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            Console.WriteLine($"You sent the Swap() method a {typeof(T)}");
+
+            T temp = a;
+
+            a = b;
+
+            b =temp;
+        }
+    }
+}
